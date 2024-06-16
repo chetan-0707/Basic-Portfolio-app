925b1e17469d97b80e2584328611774f02e5b626
Chetan Bendkule <chetanbendkule0707@gmail.com>
2024-06-16 23:16:57 +0530

    practice portfolio app

---
+++ src/data.js
@@ -0,0 +1,34 @@
+
+
+export const products = [
+  {
+    id: 1,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "https://m.media-amazon.com/images/I/61f+BoiUTGL._AC_UF1000,1000_QL80_.jpg",
+  },
+];
